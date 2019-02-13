f3a4384c75bc59a1beb687dfd2eaedb9a9be6c33
Parimal Desai <parimalvfx@gmail.com>
2019-02-13 23:38:53 +0530

    Added project stub

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+    <span>
+        Image Viewer
+    </span>, 
+    document.getElementById('root')
+);
