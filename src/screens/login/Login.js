742cc31dfc0da2f97b4d50248af99a414b69bd57
Parimal Desai <parimalvfx@gmail.com>
2019-02-16 16:57:44 +0530

    Added Header component with boilerplate code and imported Header in all screen components

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './Login.css';
+import Header from '../../common/header/Header';
 
 class Login extends Component {
     render() {
         return (
             <div>
-
+                <Header />
             </div>
         )
     }
--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './Profile.css';
+import Header from '../../common/header/Header';
 
 class Profile extends Component {
     render() {
         return (
             <div>
-
+                <Header />
             </div>
         )
     }
