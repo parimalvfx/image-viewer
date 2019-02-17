7ad708d67de78ba0123717cf51ee4dcd65a28927
Parimal Desai <parimalvfx@gmail.com>
2019-02-17 21:09:18 +0530

    Added search box and user profile icon to header for Home page

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import Input from '@material-ui/core/Input';
+import IconButton from '@material-ui/core/IconButton';
+import Avatar from '@material-ui/core/Avatar';
 
 class Header extends Component {
     render() {
         return (
             <div>
                 <header className='app-header'>
+
+                    {/* app logo */}
                     <span className='app-logo'>Image Viewer</span>
+
+                    {this.props.showProfilePicture === 'true' ?
+                        <div id='profile-picture-icon'>
+                            <IconButton>
+                                <Avatar id='profile-picture-avatar' alt='Profile picture' src='https://scontent.cdninstagram.com/vp/75dc65cfd2fa6001f7c1171f2a68c8ae/5CF96F17/t51.2885-19/s150x150/41947221_725500971134637_2241518422187835392_n.jpg?_nc_ht=scontent.cdninstagram.com' />
+                            </IconButton>
+                        </div>
+                        : ''
+                    }
+
+                    {/* search box */}
+                    {this.props.showSearchBox === 'true' ?
+                        <div className='search-box'>
+                            <SearchIcon id='search-box-icon' />
+                            <Input id='search-box-input' type='text' placeholder='Search...' disableUnderline='true' />
+                        </div>
+                        : ''
+                    }
+
                 </header>
             </div>
         )
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import './Home.css';
 import Header from '../../common/header/Header';
+import { Redirect } from 'react-router-dom';
 
 class Home extends Component {
+
+    constructor() {
+        super();
+        sessionStorage.setItem('access-token', '8661035776.d0fcd39.87fd934e04f84253aaf234d8bd4e4c65');
+        // sessionStorage.removeItem('access-token');
+        this.state = {
+        }
+    }
+
     render() {
+
+        // if a user is not logged in and tries to go to the home page by changing the URL,
+        // then the user is taken back to the login page
+        if (sessionStorage.getItem('access-token') == null) {
+            return <Redirect to='/' />
+        }
+
         return (
             <div>
-                <Header />
+                <Header showSearchBox='true' showProfilePicture='true' />
                 <h1>Home page</h1>
             </div>
         )
