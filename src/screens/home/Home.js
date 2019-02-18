450bee84ff0eb039b8cdf6a3605c41aec29db67e
Parimal Desai <parimalvfx@gmail.com>
2019-02-19 02:03:41 +0530

    Added image and horizontal rule below the image in CardContent component

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import './Home.css';
 import Header from '../../common/header/Header';
 import { Redirect } from 'react-router-dom';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
 import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
 import Avatar from '@material-ui/core/Avatar';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Divider from '@material-ui/core/Divider';
 
 const styles = theme => ({
     card: {
-        maxWidth: 400,
+        maxWidth: 500,
         marginTop: 20,
         marginLeft: 20,
     },
@@ -21,6 +23,14 @@ const styles = theme => ({
         fontWeight: 'bold',
         fontSize: 15,
     },
+    cardMedia: {
+        height: 0,
+        paddingTop: '56.25%',
+    },
+    divider: {
+        marginTop: 10,
+        // backgroundColor: '#c0c0c0'
+    }
 });
 
 class Home extends Component {
@@ -75,14 +85,31 @@ class Home extends Component {
                             title: classes.cardHeaderTitle,
                             subheader: classes.cardHeaderSubheader,
                         }}
+
+                        // card header - user profile picture
                         avatar={
                             <Avatar alt='Profile picture' src='https://scontent.cdninstagram.com/vp/75dc65cfd2fa6001f7c1171f2a68c8ae/5CF96F17/t51.2885-19/s150x150/41947221_725500971134637_2241518422187835392_n.jpg?_nc_ht=scontent.cdninstagram.com' />
                         }
+
+                        // card header - username
                         title={this.state.username}
+
+                        // card header - created date
                         subheader={this.prettyTimestamp(1538563044)}
                     />
                     <CardContent>
 
+                        {/* card content - image */}
+                        <CardMedia
+                            className={classes.cardMedia}
+                            image='http://scontent.cdninstagram.com/vp/dbac63005a92b42f4b699bcaf3d0ba3d/5CEB874D/t51.2885-15/e35/41949776_232276890980498_7193467884958027372_n.jpg?_nc_ht=scontent.cdninstagram.com'
+                            title='PG Certification in Digital Marketing & Communication\n#upgrad #marketingdigital #pgcertified'
+                        />
+
+                        {/* card content - horizontal rule */}
+                        <Divider className={classes.divider} />
+
+                        <p>PG Certification in Digital Marketing & Communication\n#upgrad #marketingdigital #pgcertified</p>
                     </CardContent>
                 </Card>
             </div>
