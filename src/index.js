df352d9a4e5ef6ae7d9aacb9d3b13dca956ae637
somijaiswal23 <somi.jaiswal@iiitb.net>
2019-02-22 03:46:37 +0530

    Profile Page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Controller from './screens/Controller';
 
 ReactDOM.render(
-    <span>
-        Image Viewer
-    </span>, 
+    <Controller />,
     document.getElementById('root')
 );
